5002aabfa350e6955b217893d9eeb2da1f97147e
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2025-02-17 13:50:48 +0530

    [Adarsh] Add. validate social security number

---
+++ Day05_RegexAndJunit/src/main/java/regex/advancedproblems/validatesocialsecuritynumber/ValidateSSN.java
@@ -0,0 +1,18 @@
+package regex.advancedproblems.validatesocialsecuritynumber;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidateSSN {
+    public static void main(String[] args) {
+        String regex = "\\b\\d{3}-\\d{2}-\\d{4}\\b";
+        String text = "My SSN is 123-45-6789.";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
